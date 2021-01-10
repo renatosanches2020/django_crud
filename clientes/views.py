22f92e3f0eaae7391fc9e3d64a9ed43f2e14d12b
renatosanches2020 <newswebmundi1@gmail.com>
2021-01-10 20:26:38 -0300

    Create : Criando Novos Registros e Integrando com List All

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
+from .forms import PersonForm
 
 def persons_list(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {'persons': persons})
+
+
+def persons_new(request):
+    form = PersonForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
