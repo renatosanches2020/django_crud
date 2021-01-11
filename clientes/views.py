04861175959fde9c8398c72e978693166c0ef559
renatosanches2020 <newswebmundi1@gmail.com>
2021-01-10 21:43:12 -0300

    Update : Atualização de Registros e Integrando com List All

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 
@@ -13,4 +13,14 @@ def persons_new(request):
     if form.is_valid():
         form.save()
         return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
+
+def persons_update(request, id):
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+
     return render(request, 'person_form.html', {'form': form})
